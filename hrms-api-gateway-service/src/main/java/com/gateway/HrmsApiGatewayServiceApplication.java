f9af492ba4936c72e8dfc0e9501e4892b4d43917
Rushikesh Doke <Rushikesh Doke@HANDT.AI>
2026-09-10 18:44:09 +0530

    Project setup

---
+++ hrms-api-gateway-service/src/main/java/com/gateway/HrmsApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsApiGatewayServiceApplication.class, args);
+	}
+
+}
