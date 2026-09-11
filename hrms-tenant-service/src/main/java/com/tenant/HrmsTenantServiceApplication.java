9c8f9d25b3ffba3a24c28f742595eb4bd0d8a30e
Rushikesh Doke <Rushikesh Doke@HANDT.AI>
2026-09-11 15:26:39 +0530

    Add new service

---
+++ hrms-tenant-service/src/main/java/com/tenant/HrmsTenantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tenant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsTenantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsTenantServiceApplication.class, args);
+	}
+
+}
